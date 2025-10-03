14a32dfc8e1ee0ed5af85c52aa417b8b02de1b37
Kutman Sulaimanov <kutman.sulaimanov@alatoo.edu.kg>
2025-10-03 16:17:12 +0600

    feat: add User entity and Role enum with Flyway migration

---
+++ booking_platform/src/main/java/com/example/booking_platform/model/entity/Role.java
@@ -0,0 +1,7 @@
+package com.example.booking_platform.model.entity;
+
+public enum Role {
+        CLIENT,
+        SPECIALIST
+
+}
---
+++ booking_platform/src/main/java/com/example/booking_platform/model/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.booking_platform.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
